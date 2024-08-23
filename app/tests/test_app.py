d65e713b5472dc102f740d670a05e62f3e8f2879
Kyrylo OLIINYK <kyrylo.oliinyk@smile-ukraine.com>
2024-08-23 15:20:12 +0300

    fix error

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -13,11 +13,6 @@ def test_return_content(client):
     assert rv.status_code == 200 or rv.status_code == 404
 
 
-def test_return_logs(client):
-    rv = client.get('logs')
-    assert rv.status_code == 200
-
-
 def test_return_status(client):
     rv = client.get('/status')
     assert rv.status_code == 200
